be9a379bb593f073248c96198649232286fca2e4
G.Joker👻 <125809602+Mateuzkl@users.noreply.github.com>
2026-06-05 15:45:12 -0300

    Color raid spawn placement warnings

--- src/raids.cpp
+++ src/raids.cpp
@@ -11,6 +11,7 @@
 #include "pugicast.h"
 #include "scheduler.h"
 #include "logger.h"
+#include <fmt/color.h>
 #include <fmt/format.h>
 
 extern Game g_game;
@@ -714,7 +715,9 @@ bool AreaSpawnEvent::executeEvent()
 			}
 
 			if (!placed) {
-				LOG_WARN(fmt::format("[Warning - AreaSpawnEvent::executeEvent] Could not place monster {} after {} tries", spawn.name, MAXIMUM_TRIES_PER_MONSTER));
+				LOG_WARN(fmt::format(fg(fmt::color::red),
+				                     "[RAIDS] Area spawn could not place monster {} after {} tries",
+				                     spawn.name, MAXIMUM_TRIES_PER_MONSTER));
 			}
 		}
 	}
