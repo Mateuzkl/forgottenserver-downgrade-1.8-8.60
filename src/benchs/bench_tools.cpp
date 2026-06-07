8ffe348da1f696b6185a84e7d1f1dab6ce1054dd
Mateuzkl <mateus.roberto@educacao.mg.gov.br>
2026-06-06 21:51:31 -0300

    fix: bench_tools.cpp unused variable warning with -Werror
    - Use [[maybe_unused]] on range-for iteration variable

--- src/benchs/bench_tools.cpp
+++ src/benchs/bench_tools.cpp
@@ -8,7 +8,7 @@ static void bench_caseInsensitiveEqual(benchmark::State& state)
 {
 	std::string a(state.range(0), 'a');
 	std::string b(state.range(0), 'A');
-	for (auto&& _ : state) {
+	for ([[maybe_unused]] auto _ : state) {
 		auto result = caseInsensitiveEqual(a, b);
 		benchmark::DoNotOptimize(result);
 	}
@@ -18,7 +18,7 @@ BENCHMARK(bench_caseInsensitiveEqual)->Range(8, 4096);
 static void bench_trimString(benchmark::State& state)
 {
 	std::string base(state.range(0), 'x');
-	for (auto&& _ : state) {
+	for ([[maybe_unused]] auto _ : state) {
 		std::string s = " \t\n" + base + " \t\n";
 		trimString(s);
 		benchmark::DoNotOptimize(s);
