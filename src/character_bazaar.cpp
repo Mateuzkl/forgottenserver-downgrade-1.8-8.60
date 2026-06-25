e192039c273c63b46d92d0000ce71adb09513f6f
G.Joker👻 <125809602+Mateuzkl@users.noreply.github.com>
2026-06-25 09:03:28 -0300

    fix: remove non-existent player->save() call in character_bazaar.cpp

--- src/character_bazaar.cpp
+++ src/character_bazaar.cpp
@@ -357,7 +357,6 @@ bool createAuction(Player* player, uint32_t startPrice, uint32_t durationSeconds
 	}
 
 	reason = fmt::format("Auction #{:d} created. You will now be logged out.", auctionId);
-	player->save();
 	const uint32_t creatureId = player->getID();
 	g_dispatcher.addTask([creatureId]() { g_game.kickPlayer(creatureId, true); });
 	return true;
