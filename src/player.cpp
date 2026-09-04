4cd6339e51c5db734a715fbeb56fd45d9031a92c
Fabian Marzan <fabi.marzan1762@yahoo.com>
2026-09-04 16:44:02 -0400

    fix: allow looking at shop items regardless of price

--- src/player.cpp
+++ src/player.cpp
@@ -5236,6 +5236,15 @@ bool Player::updateSaleShopList(const Item* item)
 	return true;
 }
 
+bool Player::hasShopItem(uint32_t itemId, uint8_t subType) const
+{
+	const ItemType& itemType = Item::items[itemId];
+	return std::any_of(shopItemList.begin(), shopItemList.end(), [&](const ShopInfo& shopInfo) {
+		return shopInfo.itemId == itemId &&
+		       (!itemType.isFluidContainer() || shopInfo.subType == subType);
+	});
+}
+
 bool Player::hasShopItemForSale(uint32_t itemId, uint8_t subType) const
 {
 	const ItemType& itemType = Item::items[itemId];
